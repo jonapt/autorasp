e1db58f86041302825d5c14932d7b33449307fb9
jonapt <jhonatanpenadt@unimagdalena.edu.co>
2024-09-02 09:36:10 -0500

    Primer commit

---
+++ auto_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Hola, Pasante Magma"
+echo "Buscando nuevos repositorios"
+sudo apt update
+clear
+echo "Actualizando todo"
+sudo apt upgrade -y
+clear
+echo "removiendo todo lo que no sirve o no se utiliza"
+sudo apt autoremove -y
+clear
+# ---------------
+
+archivo="lista_de_aplicaciones.txt"
+
+while IFS= read -r linea
+do
+    sudo apt-get install $linea -y
+    clear
+done < "$archivo"
+
+echo "Aplicaciones instalados"
+
